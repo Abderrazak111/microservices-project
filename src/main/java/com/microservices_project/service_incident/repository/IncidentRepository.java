a31940557d8a15114753940097bb764fc9b4f0be
Abderrazak Mostaid <112652189+Abderrazak111@users.noreply.github.com>
2026-02-22 17:52:08 +0100

    Delete src directory

--- src/main/java/com/microservices_project/service_incident/repository/IncidentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservices_project.service_incident.repository;
-
-import com.microservices_project.service_incident.model.Incident;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncidentRepository extends JpaRepository<Incident, Long> {
-}
